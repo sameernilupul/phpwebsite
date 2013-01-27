99ba75d5bf86d90067c4bb896ea1035d3e85210a
Sameera Nilupul <sameernilupul@gmail.com>
2013-01-27 22:49:47 +0530

    Commit

--- CodeIgniter/application/views/home.php
+++ CodeIgniter/application/views/home.php
@@ -10,31 +10,51 @@ and open the template in the editor.
     </head>
     <body>
         <div style="background: #990000;position: relative;width: 100%;height: 2100px;float: right" >
-            <div style="position: relative;width: 100%;background: #0134c5;height: 50px" >
-                
+            <div style="position: relative; width: 100%;background: #0134c5;height: 100px;float: right">
+
+                <div style="position: relative;top : 20px;width: 100px;background: bisque;height: 30px;float: left" >
+                    <form>
+                        <button type="button" onclick="alert('Sign in')">Sign in</button>
+                    </form>
+                </div>
+                <div style="position: relative;top : 20px;width: 100px;background: blue;height: 30px;float: left">
+                    <form>
+                        <button type="button" onclick="alert('Sign up')">Sign up</button>
+                    </form>
+                </div>
+                <div style="position: relative;top : 20px;width: 100px;background: blue;height: 30px;float: right">
+                    <form action="#" id="search_box">
+                        <div class="wrapper">
+                            <input type="text" id="search" name="search" placeholder="Search for Music">
+                            <button type="submit" class="search_btn"><img src="search_icon.png" title="Search"></button>
+                        </div>
+                    </form>
+
+                </div>
+
             </div>
             <div style="position: relative;width: 20%;background: #E68F8F;height: 1950px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 20%;background: aqua;height: 1950px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 20%;background: coral;height: 1950px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 20%;background: chartreuse;height: 1950px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 20%;background: goldenrod;height: 1950px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 100%;background: gray;height: 50px;float: right" >
-                
+
             </div>
             <div style="position: relative;width: 100%;background: greenyellow;height:50px;float: right" >
-                
+
             </div>
-            
+
         </div>
         <?php
         // put your code here
